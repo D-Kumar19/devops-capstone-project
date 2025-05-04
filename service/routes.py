e7d1f1236cd6da9501540cd139033438dd06da11
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2025-05-04 11:35:21 +0200

    Fixed linting issues

--- service/routes.py
+++ service/routes.py
@@ -57,10 +57,10 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -79,10 +79,10 @@ def list_accounts():
     response_status = status.HTTP_200_OK
     return jsonify(message), response_status
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id: int):
     """
@@ -102,10 +102,10 @@ def read_account(account_id: int):
 
     return jsonify(message), response_status
 
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id: int):
     """
@@ -132,7 +132,6 @@ def update_account(account_id: int):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id: int):
     """
@@ -157,8 +156,6 @@ def delete_account(account_id: int):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
